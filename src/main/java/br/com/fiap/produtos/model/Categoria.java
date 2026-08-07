0da4ccb426a96882c89c8cd2287816da2bb4b1f3
thaupereira <thauanpereiratkd@outlook.com>
2026-08-07 17:10:19 -0300

    create class categori
    criação da classe categoria

--- src/main/java/br/com/fiap/produtos/model/Categoria.java
+++ src/main/java/br/com/fiap/produtos/model/Categoria.java
@@ -1,8 +1,57 @@
 package br.com.fiap.produtos.model;
 
+import java.util.Objects;
+
 public class Categoria {
 
-    Long id;
+    private Long id;
 
-    String nome;
+    private String nome;
+
+    public Categoria() {
+    }
+
+    public Categoria(Long id, String nome) {
+        this.setId(id);
+        this.setNome(nome);
+    }
+
+    public Categoria(String nome) {
+        this.setNome(nome);
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(nome, categoria.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(nome);
+    }
+
+    @Override
+    public String toString() {
+        return nome.toUpperCase();
+    }
 }
